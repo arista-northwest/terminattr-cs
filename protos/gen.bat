b42eff8fb1ed417198ad6493cfe9943f491d7fa2
Jesse Mather <jmather@arista.com>
2018-10-24 10:35:33 -0700

    move src files around

--- protos/gen.bat
+++ protos/gen.bat
@@ -1,4 +1,4 @@
 
 @rem test
 
-%UserProfile%\.nuget\packages\grpc.tools\1.15.0\tools\windows_x86\protoc.exe -I. --csharp_out ../ --grpc_out ../ openconfig.proto --plugin=protoc-gen-grpc=%UserProfile%\.nuget\packages\grpc.tools\1.15.0\tools\windows_x86\grpc_csharp_plugin.exe
+%UserProfile%\.nuget\packages\grpc.tools\1.15.0\tools\windows_x86\protoc.exe -I. --csharp_out ../src/ --grpc_out ../src/ openconfig.proto --plugin=protoc-gen-grpc=%UserProfile%\.nuget\packages\grpc.tools\1.15.0\tools\windows_x86\grpc_csharp_plugin.exe
