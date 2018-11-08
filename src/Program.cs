aad7c39aa5c3083ca87aa4f4b1282125a598dad8
Jesse Mather <jmather@arista.com>
2018-11-07 18:40:12 -0800

    change default path

--- src/Program.cs
+++ src/Program.cs
@@ -14,6 +14,8 @@ namespace terminattr
             string dut;
             Console.Write("Enter DUT IP address - ");
             dut = Console.ReadLine();
+
+            // default DUT for testing...
             if (dut.Length == 0)
             {
                 dut = @"172.24.70.163";
@@ -22,12 +24,13 @@ namespace terminattr
             string pathString;
             Console.Write("Enter Path to subscribe to - ");
             pathString = Console.ReadLine();
+
+            // Default path for testing...
             if (pathString.Length == 0)
             {
-                pathString = @"environment/temperature/config";
+                pathString = @"/Sysdb/environment";
             }
-
-
+            
             Channel channel = new Channel(dut, 6042, ChannelCredentials.Insecure);
             gNMI.gNMIClient client = new gNMI.gNMIClient(channel);
 
@@ -39,21 +42,19 @@ namespace terminattr
                 method = @"get";
             }
 
-            //requests = []
-            //for path in paths:
-            //    path = self._to_ocpath(path)
-            //    subscription = pb.Subscription(path = path)
-            //    subls = pb.SubscriptionList(subscription = [subscription])
-            //    requests.append(pb.SubscribeRequest(subscribe = subls))
-
-            //for response in self._stub.Subscribe(requests, self._timeout):
-            //    yield response
             Path path = new Path();
 
-            path.Elem.Add(new PathElem { Name = "Sysdb" });
-            path.Elem.Add(new PathElem { Name = "sys" });
-            path.Elem.Add(new PathElem { Name = "net" });
-            path.Elem.Add(new PathElem { Name = "config" });
+            foreach (var element in pathString.Split('/').ToList())
+            {
+                string fixElement = element.Trim();
+                if (element.Contains('\\'))
+                {
+                    fixElement = element.Replace('\\', '/');
+                }
+
+                PathElem pathElem = new PathElem { Name = fixElement };
+                path.Elem.Add(pathElem);
+            }
 
             if ( method == "get")
             {
@@ -61,13 +62,11 @@ namespace terminattr
                 GetRequest getreq = new GetRequest();
                 getreq.Path.Add(path);
                 GetResponse resp = client.Get(getreq);
-                Console.WriteLine("=============================");
                 foreach (Notification item in resp.Notification)
                 {
 
                     foreach (Update upd in item.Update)
                     {
-                        Console.WriteLine("-------");
                         Console.WriteLine(upd.Path);
                         //Console.WriteLine(upd.Value.Type);
                         Console.WriteLine(upd.Value.Value_.ToStringUtf8());
@@ -81,25 +80,12 @@ namespace terminattr
                 SubscriptionList subscriptionList = new SubscriptionList
                 {
                     Mode = SubscriptionList.Types.Mode.Stream,
-                    UseAliases = false,
-                    Prefix = path
+                    UseAliases = false
                 };
 
-                Path subscriptionPath = new Path();
+                
 
-                foreach (var element in pathString.Split('/').ToList())
-                {
-                    string fixElement = element.Trim();
-                    if (element.Contains('\\'))
-                    {
-                        fixElement = element.Replace('\\', '/');
-                    }
-
-                    PathElem pathElem = new PathElem { Name = fixElement };
-                    subscriptionPath.Elem.Add(pathElem);
-                }
-
-                subscriptionList.Subscription.Add(new Subscription() { Mode = SubscriptionMode.Sample, Path = subscriptionPath });
+                subscriptionList.Subscription.Add(new Subscription() { Mode = SubscriptionMode.OnChange, Path = path });
 
                 SubscribeRequest subscribeRequest = new SubscribeRequest { Subscribe = subscriptionList };
                 IAsyncStreamReader<SubscribeResponse> responseStream = null;
@@ -118,30 +104,40 @@ namespace terminattr
 
                 while (channel.State == ChannelState.Ready)
                 {
+                    Console.WriteLine("Channel is ready...");
                     if (responseStream == null)
                     {
+                        Console.WriteLine("responseStream == null");
                         Environment.Exit(-1);
                     }
 
+                    Console.WriteLine("1");
                     if (!responseStream.MoveNext().Result)
                     {
+                        Console.WriteLine("!responseStream.MoveNext().Result");
                         Environment.Exit(-1);
                     }
 
+                    Console.WriteLine("2");
                     SubscribeResponse currResponse = responseStream.Current;
+                    Console.WriteLine("3");
                     if (currResponse.Update != null)
                     {
+                        Console.WriteLine("currResponse.Update != null");
                         var elements = currResponse.Update.Prefix.Elem;
                         var elementsOldFormat = currResponse.Update.Prefix.Element;
                         var prefixPathString = elementsOldFormat.Aggregate(string.Empty, (current, element) => current + "/" + element);
 
                         foreach (var update in currResponse.Update.Update)
                         {
+                            Console.WriteLine("got update...");
                             elementsOldFormat = update.Path.Element;
                             var updatePathString = elementsOldFormat.Aggregate(string.Empty, (current, element) => current + "/" + element);
                             Console.WriteLine("{0}:{1}", $"{prefixPathString},{updatePathString}", update.Value.Value_.ToStringUtf8());
                         }
                     }
+
+                    Console.WriteLine("Update processed...");
                 }
             }
             
